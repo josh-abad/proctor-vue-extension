c7c851a2bf72acf565e5463a61b7c98f9e082382
Joshua Abad <yeyoan@gmail.com>
2021-04-17 17:11:29 +0800

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,27 @@
+interface Activity {
+  url: string;
+  time: string;
+}
+
+const activities: Activity[] = []
+
+const addActivity = (activity: Activity) => {
+  activities.push(activity)
+  console.log(`Visited ${activity.url} at ${activity.time}`)
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Extension installed')
+  chrome.tabs.onActivated.addListener(({ tabId }) => {
+    chrome.tabs.get(tabId, ({ url }) => {
+      chrome.storage.sync.get(['tracking'], items => {
+        if (items.tracking) {
+          addActivity({
+            time: new Date().toLocaleTimeString(),
+            url: url?.split('/')[2] || 'new tab'
+          })
+        }
+      })
+    })
+  })
+})
