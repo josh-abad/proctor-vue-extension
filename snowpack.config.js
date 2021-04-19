3ca1f22072108d1a7d5d1586f0528814c5a0c264
Joshua Abad <yeyoan@gmail.com>
2021-04-19 21:02:58 +0800

    Use optimize plugin to minify build

--- snowpack.config.js
+++ snowpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     src: '/dist'
   },
   plugins: [
+    ['@snowpack/plugin-optimize'],
     '@snowpack/plugin-typescript',
     '@snowpack/plugin-postcss',
     '@jadex/snowpack-plugin-tailwindcss-jit'
