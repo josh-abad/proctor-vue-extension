3bd5d8ffb3ea0d457689b1aa204260d4c8591dcf
Joshua Abad <yeyoan@gmail.com>
2021-04-21 17:13:59 +0800

    Add type prop

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 interface AppButtonProps {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
+  type: 'button' | 'submit' | 'reset';
 }
 
-const AppButton = ({ onClick, children }: AppButtonProps): JSX.Element => {
+const AppButton = ({ onClick, children, type = 'button' }: AppButtonProps): JSX.Element => {
   return (
     <button
       className="w-full px-6 py-2 text-sm font-semibold text-white capitalize duration-300 ease-in-out rounded-lg shadow appearance-none focus:outline-none bg-gradient-to-t from-green-600 to-green-500 hover:from-green-700 hover:to-green-600"
       onClick={onClick}
-      type="button"
+      type={type}
     >
       {children}
     </button>
