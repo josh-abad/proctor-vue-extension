f2fc406c23da226d19a946c4d8e99e65acd2ce7d
Joshua Abad <yeyoan@gmail.com>
2021-04-21 17:12:51 +0800

    Make onClick prop optional

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface AppButtonProps {
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }
 
