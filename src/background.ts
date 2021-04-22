db3abc4107b1005cbd9d21db755db6f62b4a055e
Joshua Abad <yeyoan@gmail.com>
2021-04-22 18:52:58 +0800

    Fix listeners in background script

--- src/background.ts
+++ src/background.ts
@@ -7,49 +7,50 @@ const addActivity = (activity: Activity) => {
   console.log(`Visited ${activity.url} at ${activity.time}`)
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-
-  // Set badge to Proctor Vue green
-  chrome.browserAction.setBadgeBackgroundColor({ color: '#10B981' })
-
-  // Set icon depending on if user is logged in and has tracking on
-  chrome.storage.sync.get(['user'], items => {
-    chrome.browserAction.setIcon({
-      path: `./images/icon${items.user?.tracking ? '16' : '_alt'}.png`
-    })
+const updateIcon = (on: boolean) => {
+  chrome.browserAction.setIcon({
+    path: `./images/icon${on ? '16' : '_alt'}.png`
   })
+}
 
-  // Change icon when tracking has changed
-  chrome.storage.onChanged.addListener((changes) => {
-    if (changes.user) {
-      const { oldValue, newValue } = changes.user
+// Change icon when tracking has changed
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes.user) {
+    const { oldValue, newValue } = changes.user
 
-      /**
-       * User logged in (!oldValue) or user logged out (!newValue).
-       * Either way, icon should be set to 'off' variant.
-       */
-      if (!oldValue || !newValue) {
-        chrome.browserAction.setIcon({
-          path: './images/icon_alt.png'
-        })
-      } else {
-        chrome.browserAction.setIcon({
-          path: `./images/icon${newValue.tracking ? '16' : '_alt'}.png`
+    /**
+     * User logged in (!oldValue) or user logged out (!newValue).
+     * Either way, icon should be set to 'off' variant.
+     */
+    if (!oldValue || !newValue) {
+      updateIcon(false)
+    } else {
+      updateIcon(newValue.tracking)
+    }
+  }
+})
+
+// Set badge to Proctor Vue green
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.browserAction.setBadgeBackgroundColor({ color: '#10B981' })
+})
+
+// Set icon depending on if user is logged in and has tracking on
+chrome.runtime.onStartup.addListener(() => {
+  chrome.storage.sync.get(['user'], items => {
+    updateIcon(items.user?.tracking)
+  })
+})
+
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+  chrome.tabs.get(tabId, ({ url }) => {
+    chrome.storage.sync.get(['user'], items => {
+      if (items.user?.tracking) {
+        addActivity({
+          time: new Date().toLocaleTimeString(),
+          url: url?.split('/')[2] || 'new tab'
         })
       }
-    }
-  })
-
-  chrome.tabs.onActivated.addListener(({ tabId }) => {
-    chrome.tabs.get(tabId, ({ url }) => {
-      chrome.storage.sync.get(['user'], items => {
-        if (items.user?.tracking) {
-          addActivity({
-            time: new Date().toLocaleTimeString(),
-            url: url?.split('/')[2] || 'new tab'
-          })
-        }
-      })
     })
   })
 })
